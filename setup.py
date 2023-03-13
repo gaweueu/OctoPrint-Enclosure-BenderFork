2d038e8f090ae347b1ab61042e415ed0f71edd7c
Gaweł <73175663+gaweueu@users.noreply.github.com>
2023-03-13 22:11:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "enclosure"
+plugin_identifier = "enclosure-bender"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_enclosure"
+plugin_package = "octoprint_enclosure-bender"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-Enclosure"
+plugin_name = "OctoPrint-Enclosure-Bender"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "4.13.2"
